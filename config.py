8e4a6f50be34322c32ce18ea8d29ffd7b650f66c
umarwazeer <a4umarkhan@gmail.com>
2024-06-23 15:38:04 +0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,4 @@
+LINKEDIN_USERNAME = 'umarkhan580830@gmail.com'
+LINKEDIN_PASSWORD = 'u4umarkhan@580830'
+SEARCH_KEYWORDS = 'Software Engineer'
+CONNECTION_MESSAGE = 'Hi, I would like to connect with you.'
--- main.py
+++ main.py
@@ -1,93 +1,62 @@
+import argparse
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from client import LIClient
 from settings import search_keys
-import argparse
 import time
 
 
+chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("user-agent=whatever you want")
+# chrome_options.add_argument("--disable-notifications")
+chrome_options.add_argument("--incognito")
+
 def parse_command_line_args():
-    parser = argparse.ArgumentParser(description="""
-        parse LinkedIn search parameters
-        """)
-    parser.add_argument('--username', type=str, required=True, 
-        help="""
-        enter LI username
-        """)
-    parser.add_argument('--password', type=str, required=True, 
-        help="""
-        enter LI password
-        """)
-    parser.add_argument('--keyword', default=search_keys['keywords'], nargs='*', 
-        help="""
-        enter search keys separated by a single space. If the keyword is more
-        than one word, wrap the keyword in double quotes.
-        """)
-    parser.add_argument('--location', default=search_keys['locations'], nargs='*',
-        help="""
-        enter search locations separated by a single space. If the location 
-        search is more than one word, wrap the location in double quotes.
-        """)
-    parser.add_argument('--search_radius', type=int, default=search_keys['search_radius'], nargs='?', 
-        help="""
-        enter a search radius (in miles). Possible values are: 10, 25, 35, 
-        50, 75, 100. Defaults to 50.
-        """)
-    parser.add_argument('--results_page', type=int, default=search_keys['page_number'], nargs='?', 
-        help="""
-        enter a specific results page. If an unexpected error occurs, one can
-        resume the previous search by entering the results page where they 
-        left off. Defaults to first results page.
-        """)
-    parser.add_argument('--date_range', type=str, default=search_keys['date_range'], nargs='?', 
-        help="""
-        specify a specific date range. Possible values are: All, 1, 2-7, 8-14,
-        15-30. Defaults to 'All'.
-        """)
-    parser.add_argument('--sort_by', type=str, default=search_keys['sort_by'], nargs='?', 
-        help="""
-        sort results by relevance or date posted. If the input string is not 
-        equal to 'Relevance' (case insensitive), then results will be sorted 
-        by date posted. Defaults to sorting by relevance.
-        """)
-    parser.add_argument('--salary_range', type=str, default=search_keys['salary_range'], nargs='?', 
-        help="""
-        set a minimum salary requirement. Possible input values are:
-        All, 40+, 60+, 80+, 100+, 120+, 140+, 160+, 180+, 200+. Defaults
-        to All.
-        """)
-    parser.add_argument('--filename', type=str, default=search_keys['filename'], nargs='?', 
-        help="""
-        specify a filename to which data will be written. Defaults to
-        'output.txt'
-        """)
+    parser = argparse.ArgumentParser(description="Parse LinkedIn search parameters")
+    parser.add_argument('--username', type=str, required=True, help="Enter LinkedIn username")
+    parser.add_argument('--password', type=str, required=True, help="Enter LinkedIn password")
+    parser.add_argument('--keyword', default=search_keys['keywords'], nargs='*', help="Enter search keys separated by a single space")
+    parser.add_argument('--location', default=search_keys['locations'], nargs='*', help="Enter search locations separated by a single space")
+    parser.add_argument('--search_radius', type=int, default=search_keys['search_radius'], nargs='?', help="Enter a search radius (in miles)")
+    parser.add_argument('--results_page', type=int, default=search_keys['page_number'], nargs='?', help="Enter a specific results page")
+    parser.add_argument('--date_range', type=str, default=search_keys['date_range'], nargs='?', help="Specify a specific date range")
+    parser.add_argument('--sort_by', type=str, default=search_keys['sort_by'], nargs='?', help="Sort results by relevance or date posted")
+    parser.add_argument('--salary_range', type=str, default=search_keys['salary_range'], nargs='?', help="Set a minimum salary requirement")
+    parser.add_argument('--filename', type=str, default=search_keys['filename'], nargs='?', help="Specify a filename to which data will be written")
     return vars(parser.parse_args())
 
 if __name__ == "__main__":
-
     search_keys = parse_command_line_args()
 
-    # initialize selenium webdriver - pass latest chromedriver path to webdriver.Chrome()
-    driver = webdriver.Chrome('/usr/bin/chromedriver')
-    driver.get("https://www.linkedin.com/uas/login")
+    # Initialize Selenium WebDriver
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    print(chrome_options, service, )
 
-    # initialize LinkedIn web client
+    # Initialize LinkedIn web client
+    print(search_keys)
     liclient = LIClient(driver, **search_keys)
 
-    liclient.login()
+    try:
+        liclient.login()
 
-    # wait for page load
-    time.sleep(3)
+        # Wait for page load
+        time.sleep(3)
 
-    assert isinstance(search_keys["keyword"], list)
-    assert isinstance(search_keys["location"], list)
+        assert isinstance(search_keys["keyword"], list)
+        assert isinstance(search_keys["location"], list)
 
-    for keyword in search_keys["keyword"]:
-        for location in search_keys["location"]:
-            liclient.keyword  = keyword
-            liclient.location = location
-            liclient.navigate_to_jobs_page()
-            liclient.enter_search_keys()
-            liclient.customize_search_results()
-            liclient.navigate_search_results()
+        for keyword in search_keys["keyword"]:
+            for location in search_keys["location"]:
+                liclient.keyword = keyword
+                liclient.location = location
+                # Additional methods for navigating and searching jobs
+                liclient.search_jobs()
 
-    liclient.driver_quit()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        if liclient.driver:
+            liclient.driver.quit()
